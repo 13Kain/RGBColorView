52cbfa033da217d6d12851ef8f0eb03ebe75f499
Nikita Yashin <n.yashin88@icloud.com>
2022-03-06 15:10:47 +0300

    prerelease version

--- RGBColorView/ViewController.swift
+++ RGBColorView/ViewController.swift
@@ -9,11 +9,39 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var rgbView: UIView!
+    
+    @IBOutlet var redSlider: UISlider!
+    @IBOutlet var greenSlider: UISlider!
+    @IBOutlet var blueSlider: UISlider!
+    
+    @IBOutlet var redValue: UILabel!
+    @IBOutlet var blueValue: UILabel!
+    @IBOutlet var greenValue: UILabel!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        rgbView.layer.cornerRadius = 30
     }
-
-
+    
+    private func changeColor() {
+        rgbView.backgroundColor = UIColor(red: CGFloat(redSlider.value), green: CGFloat(greenSlider.value), blue: CGFloat(blueSlider.value), alpha: 1)
+    }
+    
+    @IBAction func changeRGBColor() {
+        changeColor()
+    }
+    
+    @IBAction func changeRedValue() {
+        redValue.text = String(format: "%.2f", redSlider.value)
+    }
+    
+    @IBAction func changeGreenValue() {
+        greenValue.text = String(format: "%.2f", greenSlider.value)
+    }
+    
+    @IBAction func changeBlueValue() {
+        blueValue.text = String(format: "%.2f", blueSlider.value)
+    }
 }
 
